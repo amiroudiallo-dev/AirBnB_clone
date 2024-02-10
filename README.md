ca30e1ee3a26dddacaba092cad1e94f90b4e0cb0
Chymezy <chymezy332@gmail.com>
2024-02-10 12:28:29 -0800

    adding readme update

--- README.md
+++ README.md
@@ -1,34 +1,10 @@
-<p align="center">
-  <img src="https://github.com/amiroudiallo-dev/AirBnB_clone/blob/master/asset/AirBnb.png" alt="AirBnB logo">
-</p>
+# AirBnB clone - The console
 
-<h1 align="center">AirBnB_clone</h1>
-<p align="center">An AirBnB clone.</p>
+## Description
 
----
+ ALX AirBnB Clone Software Engineer Project.
+This is the first step towards building your first full web application: the AirBnB clone. This first step is very important because you will use what you build during this project with all other following projects: HTML/CSS templating, database storage, API, front-end integration
 
-# AirBnB Clone Project
-## Part 1 - command interpreter
-### Description:
-AirBnB is a complete web application, integrating database storage(not yet implemented), a back-end API((not yet implemented), and front-end((not yet implemented) interfacing in a clone of AirBnB.
+## Usage
 
-The project currently only implements the back-end console.
-### This project will entail:
-* Putting in place a parent class (called `BaseModel`) to take care of the initialization, serialization and deserialization of future instances
-* Creating a simple flow of serialization/deserialization: Instance <-> Dictionary <-> JSON string <-> file
-* Creating all classes used for AirBnB (`User`, `State`, `City`, `Place…`) that inherit from `BaseModel`
-* Create the first abstracted storage engine of the project: File storage.
-* Create all unittests to validate all our classes and storage engine
-### How to use it:
-1. Start up the console by typing `./console`.
-2. Type `help` to see what commands are available.
-### Examples:
-```
-(hbnb)create BaseModel
-22442c9d-2df8-4ad6-9877-fbebe5dc38eb
-(hbnb)show BaseModel 22442c9d-2df8-4ad6-9877-fbebe5dc38eb
-[BaseModel] (22442c9d-2df8-4ad6-9877-fbebe5dc38eb) {'updated_at': datetime.datetime(2018, 6, 13, 14, 39, 8, 949289), 'id': '22442c9d-2df8-4ad6-9877-fbebe5dc38eb', 'created_at': datetime.datetime(2018, 6, 13, 14, 39, 8, 949072)}
-```
-## Authors :black_nib:
-* **DIALLO Amirou Yannick Wendpuire** <[amiroudiallo](https://github.com/amiroudiallo-dev)>
-* **Chymezy Josh** <[Chymezy](https://github.com/chymezy)>
+The following code example will guild you on how to run the the console.
--- models/base_model.py
+++ models/base_model.py
@@ -1,68 +1,57 @@
 #!/usr/bin/python3
+"""
+BaseModel - Module
+BaseModel Parent class,
+ """
 
 import uuid
 from datetime import datetime
 import models
 
+
 class BaseModel:
-    """ This contains Airbnb base class upon which other classes inherit from """
-
+    """
+    BaseModel class Parent class to take care of the initialization,
+    serialization and deserialization of instances
+    """
     def __init__(self, *args, **kwargs):
-        """ Initializes both instance and inherited attributes """
-        time_format = "%Y-%m-%dT%H:%M:%S.%f"
-        if kwargs:
-            for key, value in kwargs.items():
-                if key == "__class__":
-                    continue
-                elif key == "created_at" or key == "updated_at":
-                    setattr(self, key, datetime.strptime(value, time_format))
-                else:
-                    setattr(self, key, value)
-        else:
+        """Initialization of a BaseModel instance"""
+        if (len(kwargs) == 0):
             self.id = str(uuid.uuid4())
-            self.created_at = datetime.utcnow()
-            self.updated_at = datetime.utcnow()
-
-        models.storage.new(self)
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            models.storage.new(self)
+        else:
+            kwargs["created_at"] = datetime.strptime(kwargs["created_at"],
+                                                     "%Y-%m-%dT%H:%M:%S.%f")
+            kwargs["updated_at"] = datetime.strptime(kwargs["updated_at"],
+                                                     "%Y-%m-%dT%H:%M:%S.%f")
+            for key, val in kwargs.items():
+                if "__class__" not in key:
+                    setattr(self, key, val)
 
     def __str__(self):
-        """ Outputs in the string format. This method is called by either str or print """
-        return f"[{__class__.__name__}], ({self.id}), {self.__dict__}"
+        """String representation of a BaseModel instance"""
+        return ("[{}] ({}) {}".format(self.__class__.__name__,
+                                      self.id, self.__dict__))
+
+    def __repr__(self):
+        """
+            Return string representation of BaseModel class
+        """
+        return ("[{}] ({}) {}".format(self.__class__.__name__,
+                                      self.id, self.__dict__))
 
     def save(self):
-        """ This method saves the updated_at attribute with the current date """
-        self.updated_at = datetime.utcnow()
+        """updates 'updated_at' instance with current datetime"""
+        self.updated_at = datetime.now()
         models.storage.save()
 
     def to_dict(self):
-        """ This function returns a dictionary containing keys/values of the object instance """
-        obj_dict = self.__dict__.copy()
-        obj_dict["__class__"] = self.__class__.__name__
-        obj_dict["created_at"] = self.created_at.isoformat()
-        obj_dict["updated_at"] = self.updated_at.isoformat()
-        return obj_dict
+        """Return dictionary representation of BaseModel class."""
+        nw_dct = dict(self.__dict__)
+        nw_dct['__class__'] = self.__class__.__name__
+        nw_dct['created_at'] = self.updated_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
+        nw_dct['updated_at'] = self.created_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
 
-
-if __name__ == "__main__":
-
-    my_model = BaseModel()
-    my_model.name = "My First Model"
-    my_model.my_number = 89
-    print(my_model)
-    my_model.save()
-    print(my_model)
-    my_model_json = my_model.to_dict()
-    print(my_model_json)
-    print("JSON of my_model:")
-    for key in my_model_json.keys():
-        print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
-
-    print("--")
-    my_new_model = BaseModel(**my_model_json)
-    print(my_new_model.id)
-    print(my_new_model)
-    print(type(my_new_model.created_at))
-
-    print("--")
-    print(my_model is my_new_model)
-
+        return (nw_dct)
